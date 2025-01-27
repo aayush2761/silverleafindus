9de987aae96534bf9780a1f42bbad1e3b7ba01f2
Aman Kumar <trulyaman25@gmail.com>
2025-01-27 15:46:44 +0530

    Updated Footer Design

--- src/components/navigation/footer/footer.jsx
+++ src/components/navigation/footer/footer.jsx
@@ -7,21 +7,21 @@ function Footer() {
         <div className="w-full h-fit bg-[#131313] text-white px-5 sm:px-20 py-10">
             <div className="w-full h-full px-4 flex flex-col items-center justify-between gap-10">
                 <div className="flex flex-col justify-center item-center gap-4 text-center">
-                    <h1 className="font-bugetaDream text-[#FED685] text-4xl">SilverLeaf Indus</h1>
-                    <h2 className="px-7 font-saudagar text-white text-md text-center">Crafting Timeless Stories from India's Heritage</h2>
+                    <h1 className="font-bugetaDream text-[#FED685] text-4xl sm:text-5xl transition-all ease-in-out">SilverLeaf Indus</h1>
+                    <h2 className="px-7 font-saudagar text-white text-md text-center transition-all ease-in-out">Crafting Timeless Stories from India's Heritage</h2>
                 </div>
 
                 <Link to="/" className='px-3 py-2 bg-[#FED685] text-xl text-[#131313] border-2 border-[#FED685] font-megante capitalize hover:bg-transparent hover:text-white transition-all ease-in-out'>
                     explore more
                 </Link>
 
-                <div className='w-full mt-5 h-fit flex flex-col justify-between item-center gap-7'>
-                    <div className='flex flex-col justify-center items-center gap-7'>
-                        <div className='font-bugetaDream text-2xl text-[#FED685]'>
+                <div className='w-full mt-5 xl:my-5 h-fit flex flex-col 2xl:flex-row-reverse justify-between item-center gap-7 transition-all ease-in-out'>
+                    <div className='flex flex-col xl:flex-row xl:gap-16 justify-center items-center gap-7'>
+                        <div className='font-bugetaDream text-2xl text-[#FED685] '>
                             socials
                             <div className="w-full h-[2px] bg-gray-300 rounded-full"></div>
                         </div>
-                        <div className='flex flex-row justify-center item-center gap-8'>
+                        <div className='flex flex-row justify-center item-center gap-8 sm:gap-14 transition-all ease-in-out'>
                             <Link to="https://www.facebook.com/" className='w-[50px] h-[50px] border-2 border-[#FED685] hover:border-[#3b5998] rounded-full flex justify-center items-center hover:text-[#3b5998] transition'>
                                 <FaFacebook size={20} />
                             </Link>
@@ -37,29 +37,28 @@ function Footer() {
                         </div>
                     </div>
 
-                    <div className="flex flex-col justify-center items-center gap-5 mt-5">
+                    <div className="flex flex-col xl:flex-row justify-center items-center gap-5 xl:gap-16 mt-5 xl:mt-0">
                         <div className="font-bugetaDream text-2xl text-center text-[#FED685]">
                             links
                             <div className="w-full h-[2px] bg-gray-300 rounded-full"></div>
                         </div>
 
-
-                        <div className='flex flex-col justify-center items-center gap-2'>
-                            <div className="font-megante hover:text-[#FED685] flex items-center">Terms & Conditions</div>
-                            <div className="font-megante hover:text-[#FED685] flex items-center">Become a Member</div>
-                            <div className="font-megante hover:text-[#FED685] flex items-center">FAQs</div>
-                            <div className="font-megante hover:text-[#FED685] flex items-center">Privacy</div>
-                            <div className="font-megante hover:text-[#FED685] flex items-center">Contact</div>
+                        <div className='flex flex-col md:flex-row justify-center items-center gap-2 md:gap-10 transition-all ease-in-out'>
+                            <div className="font-megante hover:text-[#FED685] flex items-center cursor-pointer">Terms & Conditions</div>
+                            <div className="font-megante hover:text-[#FED685] flex items-center cursor-pointer">Become a Member</div>
+                            <div className="font-megante hover:text-[#FED685] flex items-center cursor-pointer">FAQs</div>
+                            <div className="font-megante hover:text-[#FED685] flex items-center cursor-pointer">Privacy</div>
+                            <div className="font-megante hover:text-[#FED685] flex items-center cursor-pointer">Contact</div>
                         </div>
                     </div>
                 </div>
 
-                <div className="w-full text-center flex flex-col gap-5 mt-5">
+                <div className="w-full text-center flex flex-col gap-5 mt-5 xl:mt-0">
                     <p className="font-saudagar text-white text-md">
-                        &copy; 2025 <a href="https://www.silverleafindus.vercel.app/" className='hover:text-[#FED685]'>SilverLeaf Indus.</a> | Where Every Artifact Whispers India’s Story. 🌿
+                        &copy; 2025 <a href="https://www.silverleafindus.vercel.app/" className='text-[#FED685] hover:text-emerald-300'>SilverLeaf Indus.</a> | Where Every Artifact Whispers India’s Story. 🌿
                     </p>
                     <p className="font-saudagar text-white text-md">
-                        Website Developed with ❤️ by <a href="" className='text-[#FED685] hover:text-green'>BINARY BOTS</a>
+                        Website Developed with ❤️ by <a href="" className='text-[#FED685] hover:text-emerald-300'>BINARY BOTS</a>
                     </p>
                 </div>
             </div>
