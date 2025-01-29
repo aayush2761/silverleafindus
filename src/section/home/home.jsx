1ab2b26910b1ba5cd4cc5b0e4f0c3015c9d7ac80
Aman Kumar <trulyaman25@gmail.com>
2025-01-29 16:13:52 +0530

    FeedBack Change 1

--- src/section/home/home.jsx
+++ src/section/home/home.jsx
@@ -27,24 +27,14 @@ function Home() {
     return (
         <>
             <main className="relative">
-                <div id='home' className="relative w-full h-screen">
+                <div id='home' className="relative w-full h-[70vh]">
                     <img src={TextureOne} alt="Background Texture" className="w-full h-full object-cover -z-10 filter brightness-50" />
-                    <div className="absolute inset-0 flex flex-col justify-center items-center px-5">
-                        <motion.h1
-                            className="w-full text-amber-50 text-center text-5xl sm:text-6xl md:text-7xl 2xl:text-8xl font-bugetaDream"
-                            initial={{ opacity: 0, y: -50 }}
-                            animate={{ opacity: 1, y: 0 }}
-                            transition={{ duration: 1 }}
-                        >
+                    <div className="absolute inset-0 flex flex-col justify-center items-center px-5 mt-14">
+                        <motion.h1 className="w-full text-amber-50 text-center text-5xl sm:text-6xl md:text-7xl 2xl:text-8xl font-bugetaDream" initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1 }} >
                             SilverLeaf Indus
                         </motion.h1>
 
-                        <motion.h2
-                            className="w-full text-amber-100 text-center text-lg sm:text-xl md:text-2xl 2xl:text-3xl font-saudagar mt-4"
-                            initial={{ opacity: 0, y: 50 }}
-                            animate={{ opacity: 1, y: 0 }}
-                            transition={{ duration: 1, delay: 0.2 }}
-                        >
+                        <motion.h2 className="w-full text-amber-100 text-center text-lg sm:text-xl md:text-2xl 2xl:text-3xl font-saudagar mt-4" initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, delay: 0.2 }} >
                             Crafting Timeless Stories from India's Heritage
                         </motion.h2>
 
@@ -54,10 +44,10 @@ function Home() {
                     </div>
                 </div>
 
-                <section className="relative w-full h-[60px] sm:h-[70px] md:h-[80px] lg:h-[90px] 2xl:h-[100px] bg-[#131313] flex justify-center items-center overflow-hidden transition-all ease-in-out">
+                <section className="relative w-full h-[50px] md:h-[50px] lg:h-[60px] 2xl:h-[60px] bg-[#131313] flex justify-center items-center overflow-hidden transition-all ease-in-out">
                     <div className="absolute flex animate-marquee">
-                        {Array(10).fill(bannerWords).flat().map((word, index) => (
-                            <div key={index} className="text-amber-100 text-xl sm:text-2xl md:text-3xl 2xl:text-4xl font-saudagar px-5 py-2 uppercase transition-all ease-in-out">
+                        {Array(250).fill(bannerWords).flat().map((word, index) => (
+                            <div key={index} className="text-amber-100 text-xl sm:text-2xl md:text-3xl font-saudagar px-5 py-2 uppercase transition-all ease-in-out">
                                 {word}
                             </div>
                         ))}
@@ -65,7 +55,7 @@ function Home() {
                 </section>
 
                 <section id="products" className="w-full h-auto pt-10" initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 0.5 }} viewport={{ once: true, amount: 0.3 }} >
-                    <h1 className="font-bugetaDream text-[#131313] text-center text-3xl sm:text-4xl md:text-5xl xl:text-6xl capitalize">
+                    <h1 className="font-bugetaDream text-[#131313] text-center text-3xl sm:text-4xl md:text-5xl capitalize">
                         Our Products
                     </h1>
 
@@ -73,56 +63,56 @@ function Home() {
                         <span className="w-[200px] h-[2px] bg-[#131313] rounded-full"></span>
                     </div>
 
-                    <div className="flex flex-wrap justify-center mt-20 gap-10">
+                    <div className="flex flex-wrap justify-center mt-10 gap-10">
                         {ProductDetails.map((product) => (
-                            <motion.div key={product.productID} className="bg-white w-full xs:w-[80%] sm:w-[70%] md:w-[40%] max-w-[600px] shadow-lg" initial={{ opacity: 0, y: 50 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} viewport={{ once: true, amount: 0.2 }} >
-                                <div className="relative">
-                                    <img src={product.images[0]} alt="Nike Athletic Shoe" className="w-full h-auto" />
-                                </div>
+                            <div key={product.productID} className="bg-white h-fit w-full xs:w-[80%] sm:w-[70%] md:w-[40%] max-w-[350px] shadow-lg" initial={{ opacity: 0, y: 50 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} viewport={{ once: true, amount: 0.2 }} >
+                                <Link to={`/product/${product.productID}`} className="relative overflow-hidden block">
+                                    <img src={product.images[0]} alt={`${product.name}'s image`} className="w-full h-auto transition-all duration-300 ease-in-out hover:scale-105 hover:brightness-75" />
+                                </Link>
 
                                 <div className="px-6 sm:px-10 py-10 space-y-3">
-                                    <h2 className="text-3xl font-saudagar text-gray-800">{product.name}</h2>
-                                    <h2 className="text-lg  font-saudagar text-gray-600 lowercase">{product.description}</h2>
+                                    <h2 className="text-2xl font-saudagar text-gray-800">{product.name}</h2>
+                                    <h2 className="text-md  font-saudagar text-gray-600 lowercase">{product.description}</h2>
 
                                     <div className="flex justify-between items-center">
                                         <div>
-                                            <p className="text-gray-600 text-lg font-saudagar">price</p>
-                                            <p className="text-2xl font-bold text-gray-900 font-saudagar">₹ {product.price}</p>
+                                            <p className="text-gray-600 text-md font-saudagar">price</p>
+                                            <p className="text-xl font-bold text-gray-900 font-saudagar">₹ {product.price}</p>
                                         </div>
 
-                                        <Link to={`/product/${product.productID}`} className="rounded-xl transition-colors uppercase font-megante px-4 py-2 lg:px-6 lg:py-4 border-2 border-[#FED685] bg-[#FED685] hover:bg-transparent hover:text-[#131313] text-black text-sm"> view details </Link>
+                                        <Link to={`/product/${product.productID}`} className="rounded-xl transition-colors uppercase font-megante px-4 py-2 lg:px-3 lg:py-2 border-2 border-[#FED685] bg-[#FED685] hover:bg-transparent hover:text-[#131313] text-black text-sm"> view details </Link>
                                     </div>
                                 </div>
-                            </motion.div>
+                            </div>
                         ))}
                     </div>
                 </section>
 
                 <section id="about" className="w-full mt-10" initial={{ opacity: 0, y: 50 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} viewport={{ once: true, amount: 0.3 }} >
-                    <div className="w-full h-[90px] lg:h-[125px] bg-[#131313] flex flex-row justify-center items-center">
-                        <h1 className="text-[#FED685] font-bugetaDream text-3xl sm:text-4xl md:text-5xl xl:text-6xl uppercase mt-4">
+                    <div className="w-full h-[90px] bg-[#131313] flex flex-row justify-center items-center">
+                        <h1 className="text-[#FED685] font-bugetaDream text-3xl sm:text-4xl md:text-5xl uppercase mt-4">
                             About Us
                         </h1>
                     </div>
 
                     <motion.div className="px-8 py-10 xs:px-16 lg:px-24 xl:px-28 2xl:px-44 flex flex-col items-center" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.1 }} >
-                        <motion.h2 className="text-3xl sm:text-4xl md:text-5xl font-megante text-gray-800 mb-6"  initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.1 }} >
+                        <motion.h2 className="text-3xl sm:text-4xl font-megante text-gray-800 mb-6"  initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.1 }} >
                             Welcome to Silverleaf Indus
                         </motion.h2>
-                        <motion.p className="text-gray-600 text-lg sm:text-xl font-saudagar leading-8 mb-4" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.2 }} >
+                        <motion.p className="text-gray-600 text-lg sm:text-md font-saudagar leading-8 mb-4" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.2 }} >
                             At Silverleaf Indus, we are passionate about preserving and sharing the rich heritage of Indian artistry.
                             Our carefully curated collection includes stunning Indian artifacts that celebrate the craftsmanship and
                             traditions passed down through generations. From exquisite metallic horses to intricately designed boats, 
                             each piece tells a unique story of artistry and culture.
                         </motion.p>
 
-                        <motion.p className="text-gray-600 text-lg sm:text-xl font-saudagar leading-8 mb-4" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.3 }} >
+                        <motion.p className="text-gray-600 text-lg sm:text-md font-saudagar leading-8 mb-4" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.3 }} >
                             Our mission is to connect collectors, enthusiasts, and admirers of Indian artifacts with timeless treasures 
                             that inspire and captivate. Whether you're seeking to add a touch of elegance to your space or looking 
                             for a meaningful gift, our collection offers something truly special.
                         </motion.p>
 
-                        <motion.p className="text-gray-600 text-lg sm:text-xl font-saudagar leading-8" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.4 }} >
+                        <motion.p className="text-gray-600 text-lg sm:text-md font-saudagar leading-8" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.4 }} >
                             Join us on a journey through the cultural legacy of India, and let Silverleaf Indus bring a piece of this 
                             incredible heritage into your life.
                         </motion.p>
@@ -131,10 +121,7 @@ function Home() {
                             <button className="font-megante px-10 py-4 bg-[#131313] border-2 border-[#131313] text-[#FED685] hover:bg-transparent hover:text-[#131313] transition-all ease-in-out capitalize hover:cursor-pointer">
                                 Join Us
                             </button>
-                            <NavLink
-                                onClick={handleProductClick}
-                                className="font-megante px-10 py-4 bg-[#131313] border-2 border-[#131313] text-[#FED685] hover:bg-transparent hover:text-[#131313] transition-all ease-in-out capitalize hover:cursor-pointer"
-                            >
+                            <NavLink onClick={handleProductClick} className="font-megante px-10 py-4 bg-[#131313] border-2 border-[#131313] text-[#FED685] hover:bg-transparent hover:text-[#131313] transition-all ease-in-out capitalize hover:cursor-pointer" >
                                 Shop Now
                             </NavLink>
                         </div>
@@ -143,24 +130,24 @@ function Home() {
                     <span className="w-[300px] sm:w-[400px] md:w-[500px] lg:w-[600px] xl:w-[700px] h-[1px] md:h-[2px] mt-10 md:mt-20 rounded-full bg-gradient-to-r from-gray-100 via-[#131313] to-gray-100 shadow-md"></span>
                 </section>
 
-                <section id="team" className="w-full mt-10" >
+                <section id="team" className="w-full" >
                     <motion.div className="px-8 py-10 xs:px-16 lg:px-24 xl:px-28 2xl:px-44 flex flex-col items-center">
-                        <motion.h2 className="text-3xl sm:text-4xl md:text-5xl font-megante text-gray-800 mb-6 mt-10 md:mt-20 capitalize" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.2 }} >
+                        <motion.h2 className="text-3xl sm:text-4xl font-megante text-gray-800 mb-6 mt-10 md:mt-20 capitalize" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.2 }} >
                             Meet the Team
                         </motion.h2>
 
-                        <motion.p className="text-gray-600 text-lg sm:text-xl font-saudagar leading-8 mb-4"  initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.2 }} >
+                        <motion.p className="text-gray-600 text-lg sm:text-md font-saudagar leading-8 mb-4"  initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.2 }} >
                             Behind every success is a group of talented and dedicated individuals working tirelessly to bring ideas to life. 
                             Our team is the heartbeat of our brand, blending expertise, creativity, and passion to achieve excellence.
                             Get to know the people who make it all possible.
                         </motion.p>
 
                         <div className="py-10 lg:px-24 xl:px-28 2xl:px-44">
-                            <motion.div className="grid grid-cols-1 md:grid-cols-2 gap-20 mt-10 w-full xl:max-w-4xl 2xl:max-w-5xl" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.3 }} >
+                            <motion.div className="grid grid-cols-1 md:grid-cols-2 gap-20 w-full" initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.3 }} >
                                 {teamData.map((member, index) => (
-                                    <div key={index} className="bg-white rounded-lg overflow-hidden shadow-lg relative group">
-                                        <div className="relative w-full h-[0px] xl:h-[300px] 2xl:h-[400px] overflow-hidden">
-                                            <img src={member.img} alt={member.name} className="w-full h-full object-cover object-center transition-transform duration-500 ease-in-out transform hover:scale-105 hover:shadow-xl" />
+                                    <div key={index} className="bg-white w-full xs:w-[80%] sm:w-[70%] md:w-[100%] max-w-[350px] mx-auto overflow-hidden shadow-lg relative group">
+                                        <div className="relative w-full h-auto overflow-hidden">
+                                            <img src={member.img} alt={member.name} className="w-full h-auto object-cover object-center transition-transform duration-500 ease-in-out transform hover:scale-105 hover:brightness-75" />
                                             <div className="absolute inset-0 flex justify-center items-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                                                 <div className="flex space-x-10">
                                                     {member.socialLinks.map((link, idx) => (
@@ -171,7 +158,7 @@ function Home() {
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className="px-10 py-14 text-center">
+                                        <div className="px-7 py-7 text-center">
                                             <h2 className="text-xl font-megante text-gray-800 uppercase font-albulaBold tracking-widest">{member.name}</h2>
                                             <h3 className="text-lg text-gray-600 mt-1 font-saudagar">- {member.role}</h3>
                                             <p className="text-gray-500 mt-4 font-saudagar text-md">{member.description}</p>
